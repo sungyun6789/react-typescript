e179a46cf20c3eccda1ff9bab2e60446aa5ff742
sungyun <sungyun5423@gmail.com>
2021-05-16 17:58:00 +0900

    refactor: typesafe-actions를 사용한 리팩토링

--- src/todo/modules/counter.ts
+++ src/todo/modules/counter.ts
@@ -1,34 +1,22 @@
+import { createAction, ActionType, createReducer, action } from 'typesafe-actions';
+
 // Action Type
 // 뒤에 as count를 붙어줌으로써 나중에 액션 객체를 만들때 action.type의 값을 추론하는 과정에서
 // action.type 이 string 으로 추론되지 않고 'counter/INCREASE'와 같이 실제 문자열 값으로 추론 되도록 해줌
-const INCREASE = 'counter/INCREASE' as const;
-const DECREASE = 'counter/DECREASE' as const;
-const INCREASE_BY = 'counter/INCREASE_BY' as const;
+const INCREASE = 'counter/INCREASE';
+const DECREASE = 'counter/DECREASE';
+const INCREASE_BY = 'counter/INCREASE_BY';
 
 // 액션 생성함수를 선언함
-export const increase = () => ({
-  type: INCREASE,
-});
-
-export const decrease = () => ({
-  type: DECREASE,
-});
-
-export const increaseBy = (diff: number) => ({
-  type: INCREASE_BY,
-  // 액션에 부가적으로 필요한 값을 payload라는 이름으로 통일
-  // 이는 FSA라는 규칙으로 이 규칙을 이용하면 액션들이 모두 비슷한 구조로 이루어져있게 되어 추후에 다룰 때도 편하고
-  // 읽기 쉽고, 액션 구조를 일반화함으로써 액션에 관련된 라이브러리를 사용할 수 있게 해줌
-  payload: diff,
-});
+export const increase = createAction(INCREASE)();
+export const decrease = createAction(DECREASE)();
+export const increaseBy = createAction(INCREASE_BY)<number>(); // payload 타입을 Generics으로 설정
 
 // 모든 액션 객체들에 대한 타입을 준비
 // ReturnType<typeof ____>는 특정 함수의 반환값을 추론
 // 상단부에서 액션타입을 선언 할 때 as const를 하지 않으면 이 부분이 제대로 동작하지 않음
-type CounterAction =
-  | ReturnType<typeof increase>
-  | ReturnType<typeof decrease>
-  | ReturnType<typeof increaseBy>;
+const actions = { increase, decrease, increaseBy }; // 모든 액션 생성함수들을 actions 객체에 넣음
+type CounterAction = ActionType<typeof actions>; // ActionType를 사용하여 모든 액션 객체들의 타입을 준비
 
 // 이 리덕스 모듈에서 관리 할 상태의 타입을 선언
 type CounterState = {
@@ -41,25 +29,13 @@ const initialState: CounterState = {
 };
 
 // 리듀서를 작성
-// 리듀서에서는 state와 함수의 반환값이 일치하도록 작성
-// 액션에서는 우리가 방금 만든 CounterAction 을 타입으로 설정
-function counter(state: CounterState = initialState, action: CounterAction): CounterState {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        count: state.count + 1,
-      };
-    case DECREASE:
-      return {
-        count: state.count - 1,
-      };
-    case INCREASE_BY:
-      return {
-        count: state.count + action.payload,
-      };
-    default:
-      return state;
-  }
-}
+// createReducer는 리듀서를 쉽게 작성할 수 있게 하는 함수
+// createReducer는 switch문이 아닌 객체 형태로 작성 할 수 있게 해줌(훨씬 간결)
+// Generics로 리듀서에서 관리할 상태, 그리고 리듀서에서 처리 할 모든 액션 객체들의 타입을 넣음
+const counter = createReducer<CounterState, CounterAction>(initialState, {
+  [INCREASE]: (state) => ({ count: state.count + 1 }), // 액션을 참조 할 필요 없으면 파라미터로 state만 받아와도 됨
+  [DECREASE]: (state) => ({ count: state.count - 1 }),
+  [INCREASE_BY]: (state, action) => ({ count: state.count + action.payload }), // 액션의 타입을 유추할 수 있음
+});
 
 export default counter;
